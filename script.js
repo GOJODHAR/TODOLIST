dea196813f4c090465487cebd79e899ad993523b
GOJODHAR <loukikkadam75@gmail.com>
2025-11-27 13:11:24 +0530

    Initial commit - ToDoList Project

---
+++ script.js
@@ -0,0 +1,13 @@
+// const input = document.getElementById('taskInput');
+// const addButton = document.getElementById('addTaskButton');
+// const todoList = document.getElementById('todo_list');
+
+// addButton.addEventListener('click', () => {
+
+//     if(input.value.trim() === '') return;
+
+//     const liItem = document.createElement('li');
+//     liItem.textContent = input.value;
+//     todoList.appendChild(liItem);
+//     input.value = '';
+// })
